269b0d2dbec2035bf060cf18131a7fcba7a6f49b
Lindsay Knapp <lindsayknapp@lawn-128-61-9-28.lawn.gatech.edu>
2018-04-17 13:22:55 -0400

    inital code prototype

---
+++ ExcelJuniorDesign/ExcelJuniorDesign/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExcelJuniorDesign
+//
+//  Created by Lindsay Knapp on 4/16/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: Properties
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
